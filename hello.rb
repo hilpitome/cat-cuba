49abd06e7feed5126dcf529476eae699d23e407f
hilpitome <hilpitome@gmail.com>
2015-08-20 23:45:17 +0300

    passed a block to cuba

--- hello.rb
+++ hello.rb
@@ -1,2 +1,10 @@
 require "cuba"
 require "rack/protection"
+Cuba.use Rack::Session::Cookie, :secret => "__a_very_long_string__"
+Cuba.use Rack::Protection
+
+Cuba.define do
+  on get do
+    on "hello" do
+      res.write "Hello world!"
+    end
